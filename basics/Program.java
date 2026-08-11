5ffe9a4838812ab023e2a682a9f02eae907d185d
Gowthamigoturu <gowthamigoturu@gmail.com>
2026-08-11 14:37:57 +0530

    array iterating

---
+++ basics/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+    public static void main(String[] args) {
+        // Adding sample data to an integer array
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        System.out.println("Array elements are:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
